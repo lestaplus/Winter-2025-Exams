a208c1a40c352ded12947966c41dc9563ab60f48
lestaplus <shevtsov190107@gmail.com>
2024-12-25 23:31:24 +0200

    avoiding magic numbers

--- Tasks/day.js
+++ Tasks/day.js
@@ -4,10 +4,12 @@
 'use strict';
 
 const DAYS = ['sun', 'mon', 'tue', 'wed', 'thu', 'fri', 'sat'];
+const DAY_OFFSET = 1;
+const NOT_FOUND = -1;
 
 const parseDay = (str) => {
   const index = DAYS.findIndex((day) => str.startsWith(day));
-  return index !== -1 ? index + 1 : -1;
+  return index !== NOT_FOUND ? index + DAY_OFFSET : NOT_FOUND;
 }
 
 module.exports = parseDay;
