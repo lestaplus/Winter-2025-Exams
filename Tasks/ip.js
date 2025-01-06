a08ca21c7fe09db0e3a8bff6f6ea5cc08c48ce36
lestaplus <shevtsov190107@gmail.com>
2025-01-07 00:34:30 +0200

    avoiding magic numbers, follow code style

--- Tasks/ip.js
+++ Tasks/ip.js
@@ -3,16 +3,21 @@
 
 'use strict';
 
+const IP_PARTS_COUNT = 4;
+
 const parseIP = (input) => {
   if (!input) return;
+
   const result = [];
-    const parts = input.split('.');
-    if (parts.length !== 4) return;
-    for (const part of parts) {
-      const number = parseInt(part, 10);
-      if (isNaN(number)) return;
-      result.push(number);
-    }
+  const parts = input.split('.');
+  if (parts.length !== IP_PARTS_COUNT) return;
+
+  for (const part of parts) {
+    const number = parseInt(part, 10);
+    if (isNaN(number)) return;
+    result.push(number);
+  }
+
   return result;
 };
 
