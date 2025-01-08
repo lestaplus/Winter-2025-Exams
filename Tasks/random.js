3c982505cf24340f608e50c54160f1527e654aa9
lestaplus <shevtsov190107@gmail.com>
2025-01-08 22:47:37 +0200

    add ternary operator, use prettier

--- Tasks/random.js
+++ Tasks/random.js
@@ -3,13 +3,9 @@
 
 'use strict';
 
-const random = (min, max) => {
-  if (max === undefined) {
-    max = min;
-    return Math.floor(Math.random() * (max + 1));
-  } else {
-    return min + Math.floor(Math.random() * (max - min + 1));
-  }
-};
+const random = (min, max) =>
+  max === undefined
+    ? Math.floor(Math.random() * (min + 1))
+    : min + Math.floor(Math.random() * (max - min + 1));
 
 module.exports = random;
