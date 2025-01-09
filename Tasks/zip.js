54e2fbd5475208aa02769c64c98f5810b5284653
lestaplus <shevtsov190107@gmail.com>
2025-01-09 17:25:04 +0200

    add 'use strict', 'let' and 'const'

--- Tasks/zip.js
+++ Tasks/zip.js
@@ -1,11 +1,13 @@
 // Refactor following solution
 // Zip two arrays, [1, 2] and [3, 4] -> [[1, 3], [2, 4]]
 
+'use strict';
+
 const zip = function (a = [], b = []) {
   let i = 0;
-  j = 0;
-  for (x of b) {
-    CELL = [a[i++], x];
+  let j = 0;
+  for (const x of b) {
+    const CELL = [a[i++], x];
     if (i < j) {
       delete a[i++];
     } else {
