fed536f36721e32f3365492e66cd93ab6ae70010
lestaplus <shevtsov190107@gmail.com>
2025-01-10 22:11:05 +0200

    improve naming, replace '==' with '==='

--- Tasks/section.js
+++ Tasks/section.js
@@ -3,13 +3,13 @@
 
 'use strict';
 
-const SectionString = (s, t) => {
-  const i = s.indexOf(t);
-  if (i < 0 || t == '') {
-    return [s, ''];
+const sectionOfString = (str, separator) => {
+  const index = str.indexOf(separator);
+  if (index < 0 || separator === '') {
+    return [str, ''];
   } else {
-    return [s.slice(0, i), s.slice(i + t.length)];
+    return [str.slice(0, index), str.slice(index + separator.length)];
   }
 };
 
-module.exports = SectionString;
+module.exports = sectionOfString;
