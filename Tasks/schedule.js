7e93141da1e241fc091a26be6f458501049279aa
lestaplus <shevtsov190107@gmail.com>
2025-01-12 20:27:02 +0200

    implement function using promise, use prettier

--- Tasks/schedule.js
+++ Tasks/schedule.js
@@ -3,6 +3,11 @@
 // Implement following task
 // Run `func(n)`; `n`-times with `interval` and pass `n` to `func`
 
-const schedule = async (func, n, interval) => {};
+const schedule = async (func, n, interval) => {
+  for (let i = 0; i < n; i++) {
+    await new Promise((resolve) => setTimeout(resolve, interval));
+    func(i);
+  }
+};
 
 module.exports = schedule;
